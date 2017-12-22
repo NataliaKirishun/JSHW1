a0185bcea9c4aa61ab6328a4520c4a809bbfa8bd
NataliaKirishun <31582169+NataliaKirishun@users.noreply.github.com>
2017-12-22 22:54:25 +0300

    Add files via upload

---
+++ HW1/JS.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+    printDuplicateNumbers(1, 2, 5, 8, 7, 2, 1, 8, 12, 8, 8, 8, 8, 9, -5, 1, 8, -5, 12, 12, 12, -5, 0, 0, 0,125,856,125);
+    function printDuplicateNumbers(...args) {
+        let array = [...arguments];
+        array.sort(function (a, b) {
+            return a - b;
+        });
+        let count = 1;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === array[i + 1]) {
+                count += 1;
+                if ((array[i] !== array[i + 2]) && (count > 1)) {
+                    console.log(`${array[i]}-${count}`);
+                    count = 1;
+                }
+            }
+        }
+    }
+}());
